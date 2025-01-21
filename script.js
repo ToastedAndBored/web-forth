95cbe86f28885126876c214379670c223c236ea3
Toast <bored@toasted.me>
2025-01-22 02:16:15 +0400

    Add StackExplorer class

--- script.js
+++ script.js
@@ -29,6 +29,65 @@ class Output {
 
 }
 
+class StackExplorer {
+
+  #stackArr = []
+  #stack = []
+  #callback = () => {}
+  #stackObj
+
+
+  constructor(stackObj,callback) {
+    this.#stackObj = stackObj
+    this.#callback = callback
+  }
+
+  push(value) {
+    this.#stack.unshift(value)
+    const span = document.createElement('span')
+    span.setAttribute('class', "highlighted")
+    this.#stackObj.prepend(span)
+    this.#stackArr.unshift(span)
+
+    span.insertAdjacentHTML("afterbegin", `
+      <span></span><span> ${value}</span><br>
+      `
+    )
+    this.fixNumbs()
+
+    span.addEventListener("mouseenter", (event) => {
+      this.#callback(value)
+    })
+
+    span.addEventListener("mouseleave", (event) => {
+      this.#callback(null)
+    })
+  }
+
+  pop() {
+    const value = this.#stack.shift()
+    this.#stackObj.children[0].remove()
+    this.#stackArr.shift()
+    this.fixNumbs()
+    return value
+  }
+
+  fixNumbs() {
+    for (var i in this.#stackArr) {
+      this.#stackArr[i].children[0].textContent=`${i}`
+    }
+  }
+
+  unhighlight() {
+    for (var i in this.#stackArr) {
+      this.#stackArr[i].setAttribute('class'," ")
+    }
+  }
+}
+
 let outp = new Output()
+
+const leftStackObj = document.querySelector('#left_stack')
+let leftStack = new StackExplorer(leftStackObj, console.log)
 
 outp.print('asdsadada')
