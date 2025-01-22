d2a364c4c3639decc93ab6602c8bc26f205af380
Toast <bored@toasted.me>
2025-01-22 23:57:22 +0400

    Add Dictionary class

--- script.js
+++ script.js
@@ -85,9 +85,84 @@ class StackExplorer {
   }
 }
 
+class Dictinary {
+
+  #dictionary = []
+  #dictObj
+  #dictArr = []
+  #callback = () => {}
+
+  constructor(dictObj, callback) {
+    this.#dictObj = dictObj
+    this.#callback = callback
+  }
+
+  define(word, definition, comment) {
+    if (typeof definition !== "function") {
+      definition = parseInt(definition, 10)
+    }
+    const dictElem = {"word":word, "def": definition, "comm":comment }
+    this.#dictionary.unshift(dictElem)
+    const span = document.createElement('span')
+    span.setAttribute('class', "highlighted")
+    this.#dictObj.prepend(span)
+    this.#dictArr.unshift(span)
+    span.insertAdjacentHTML("afterbegin", `
+      <span>${word} </span>
+      <span>${typeof definition === "function" ? "builtIn:" : definition} </span>
+      <span>${comment}</span><br>
+      `
+    )
+    this.fixlight(true)
+  }
+
+  fixlight(should) {
+    let seen = new Set()
+    for (var i in this.#dictArr) {
+      if (seen.has(this.#dictionary[i].word)) {
+        this.#dictArr[i].setAttribute("class","halflight")
+      }else if(i == 0 && should) {
+        this.#dictArr[i].setAttribute("class","highlighted")
+      } else {
+        this.#dictArr[i].setAttribute("class"," ")
+      }
+      seen.add(this.#dictionary[i].word)
+    }
+  }
+
+  undefine(word) {
+    for (var i in this.#dictionary) {
+      if (this.#dictionary[i].word == word) {
+        this.#dictionary.splice(i,1)
+        this.#dictArr[i].remove()
+        this.#dictArr.splice(i,1)
+        this.fixlight(false)
+        return
+      }
+    }
+    throw new Error("Cannot undefine word with no difinition")
+  }
+
+  resolve(word) {
+    for (var i in this.#dictionary) {
+      if (this.#dictionary[i].word == word) {
+        return this.#dictionary[i].def
+      }
+    }
+  }
+
+  unhighlight() {
+    this.fixlight(false)
+  }
+
+
+}
+
 let outp = new Output()
 
 const leftStackObj = document.querySelector('#left_stack')
 let leftStack = new StackExplorer(leftStackObj, console.log)
+const dictObj = document.querySelector('#dictionary')
+const dictionary = new Dictinary(dictObj, console.log())
 
 outp.print('asdsadada')
